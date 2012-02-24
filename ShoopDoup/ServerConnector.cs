6111fdbcadc6048965e84f1433b70af137cf32b3
Bryce Kam <brycekam@gmail.com>
2012-02-24 00:34:56 -0800

    Merging minigame factory classes

---
+++ ShoopDoup/ServerConnector.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace ShoopDoup
+{
+    class ServerConnector
+    {
+        private String url;
+
+        public ServerConnector()
+        {
+            url = "http://www.tomhschmidt.com/cs247/api.php";
+        }
+
+
+        public JObject makeRequest(String requestType, String projectType, String projectId)
+        {
+            StringBuilder sb = new StringBuilder();
+            byte[] buf = new byte[8192];
+
+            HttpWebRequest request;
+
+            if (requestType == "projectType")
+            {
+                request = (HttpWebRequest)WebRequest.Create(url + "?projectType=" + projectType);
+            }
+            else if (requestType == "projectId")
+            {
+                request = (HttpWebRequest)WebRequest.Create(url + "?projectId=" + projectId);
+            }
+            else
+            {
+                return null;
+            }
+
+            HttpWebResponse response = (HttpWebResponse)
+                request.GetResponse();
+
+            Stream resStream = response.GetResponseStream();
+
+            string tempString = null;
+            int count = 0;
+
+            do
+            {
+                count = resStream.Read(buf, 0, buf.Length);
+
+                if (count != 0)
+                {
+                    tempString = Encoding.ASCII.GetString(buf, 0, count);
+                    sb.Append(tempString);
+                }
+            }
+            while (count > 0);
+
+            Console.WriteLine(sb.ToString());
+
+            //return null;
+            return JObject.Parse(sb.ToString());
+
+        }
+
+    }
+}
