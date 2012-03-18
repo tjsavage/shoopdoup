69c661fe750b0307723307f0d880752a3fbba15b
Tom Schmidt <schmidt.9090@gmail.com>
2012-03-18 15:44:54 -0700

    Added new resources

--- ShoopDoup/Properties/Resources.Designer.cs
+++ ShoopDoup/Properties/Resources.Designer.cs
@@ -88,6 +88,13 @@ namespace ShoopDoup.Properties {
             }
         }
         
+        internal static System.Drawing.Bitmap blueText {
+            get {
+                object obj = ResourceManager.GetObject("blueText", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
         internal static System.IO.UnmanagedMemoryStream bobblepopwav {
             get {
                 return ResourceManager.GetStream("bobblepopwav", resourceCulture);
@@ -209,10 +216,10 @@ namespace ShoopDoup.Properties {
         internal static System.Drawing.Bitmap ThreeDigitGreen {
             get {
                 object obj = ResourceManager.GetObject("ThreeDigitGreen", resourceCulture);
-		return ((System.Drawimg.Bitmap)(obj));
-		}
-	}
-
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
         internal static System.Drawing.Bitmap ThreeDigitRed {
             get {
                 object obj = ResourceManager.GetObject("ThreeDigitRed", resourceCulture);
@@ -227,13 +234,20 @@ namespace ShoopDoup.Properties {
             }
         }
         
+        internal static System.Drawing.Bitmap TwoDigitBlue {
+            get {
+                object obj = ResourceManager.GetObject("TwoDigitBlue", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
         internal static System.Drawing.Bitmap TwoDigitGreen {
             get {
                 object obj = ResourceManager.GetObject("TwoDigitGreen", resourceCulture);
-		return ((System.Drawing.Bitmap)(obj));
-		}
-	}
-
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
         internal static System.Drawing.Bitmap TwoDigitRed {
             get {
                 object obj = ResourceManager.GetObject("TwoDigitRed", resourceCulture);
